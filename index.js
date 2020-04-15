c4f378cead0b0d63e4d541da04385a8f5c670f60
Simon Kilroy <simonkilroy1@gmail.com>
2020-04-15 14:17:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ function checkCollision(rock) {
     if (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||
         (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
         (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge) {
-      
-      return true
+
+      return true;
     } else {
-      return false
+      return false;
     }
   }
 }
