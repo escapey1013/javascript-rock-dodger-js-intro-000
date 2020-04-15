a8abcb1dda59f16dd3913cd5a9500806212f197a
Simon Kilroy <simonkilroy1@gmail.com>
2020-04-15 13:22:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function checkCollision(rock) {
                * 3. The rock's left edge is < the DODGER's right edge,
                *    and the rock's right edge is > the DODGER's right edge.
                */
-      console.log("Collision")
+      alert("Collision");
     }
   }
 }
